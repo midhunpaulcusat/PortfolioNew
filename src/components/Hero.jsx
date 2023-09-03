19ac08e69a44fa2f1e3b64bf64c54c026096bd68
Midhun Paul <midhunpaul396@gmail.com>
2023-09-03 00:31:17 -0400

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'></section>
+  )
+}
+
+export default Hero
