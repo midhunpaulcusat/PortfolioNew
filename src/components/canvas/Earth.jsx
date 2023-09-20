1ca600ffbceb7b83245feb15b58f8bbc96fe8a72
Midhun Paul <midhunpaul396@gmail.com>
2023-09-19 20:03:32 -0400

    model removed

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -8,7 +8,7 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2.5} position-y={0.001} rotation-y={0} />
   );
 };
 
@@ -23,7 +23,7 @@ const EarthCanvas = () => {
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6],
+        position: [-3, 3, 6],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
