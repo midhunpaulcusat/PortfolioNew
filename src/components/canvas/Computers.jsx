734e8184df14be5b4d58359d7696559cebc2a3a3
Midhun Paul <midhunpaul396@gmail.com>
2024-06-24 18:21:44 -0400

    phoenix bird

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,27 @@
 import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { AnimationMixer } from "three";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const { scene, animations } = useGLTF("./desktop_pc/scene.gltf");
+  const [mixer] = useState(() => new AnimationMixer(scene));
+
+  useEffect(() => {
+    if (animations.length) {
+      const action = mixer.clipAction(animations[0]);
+      action.play();
+    }
+    return () => animations.forEach((clip) => mixer.uncacheClip(clip));
+  }, [animations, mixer]);
+
+  useFrame((state, delta) => mixer.update(delta));
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.35} groundColor='black' />
+      <hemisphereLight intensity={0.35} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -21,9 +33,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={2} />
       <ambientLight intensity={0.5} />
       <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.69}
-        position={isMobile ? [0, -3, -2.2] : [0.01, -3.25, -1.5]}
+        object={scene}
+        scale={isMobile ? 0.7 : 0.009}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -37,7 +49,7 @@ const ComputersCanvas = () => {
     // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
+    // Set the initial value of the isMobile state variable
     setIsMobile(mediaQuery.matches);
 
     // Define a callback function to handle changes to the media query
@@ -56,7 +68,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
